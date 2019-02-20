1813ad33d3b6012a3a634ce0c0abb6acbbcae2b8
Olaf Dlugosz <olafdlugosz@gmail.com>
2019-02-20 10:58:51 +0100

    Add project files.

---
+++ Dijkstra/Graph.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dijkstra
+{
+    public class Graph<T>
+    {
+        public Dictionary<string, Node> Vertexes { get; set; }
+        public List<KeyValuePair<string, int>> Distances { get; set; }
+        public List<KeyValuePair<string, int>> PriorityQueue { get; set; }
+        public List<KeyValuePair<string, string>> Previous { get; set; }
+        public Graph() {
+            Vertexes = new Dictionary<string, Node>();
+            Distances = new List<KeyValuePair<string, int>>();
+            PriorityQueue = new List<KeyValuePair<string, int>>();
+            Previous = new List<KeyValuePair<string, string>>();
+        }
+        public class Node
+        {
+            public string Name { get; set; }
+            public List<KeyValuePair<string, int>> Edges { get; set; }
+
+            public Node(string name) {
+                Name = name;
+                Edges = new List<KeyValuePair<string, int>>();
+            }
+        }
+
+        public void AddVertex(string Name) {
+            var node = new Node(Name);
+            if (!Vertexes.ContainsKey(Name) && Vertexes != null) {
+                Vertexes.Add(Name, node);
+            };
+        }
+        public void AddEdge(string V1, string V2, int weight) {
+            foreach (var node in Vertexes.Values) {
+                if (node.Name == V1) node.Edges.Add(new KeyValuePair<string, int>(V2, weight));
+                if (node.Name == V2) node.Edges.Add(new KeyValuePair<string, int>(V1, weight));
+            }
+        }
+        #region Dijkstra
+        public void Dijkstra(string start, string end) {
+            //Initial State
+            foreach (var node in Vertexes.Values) {
+                if (node.Name == start) {
+                    Distances.Add(new KeyValuePair<string, int>(node.Name, 0));
+                    PriorityQueue.Add(new KeyValuePair<string, int>(node.Name, 0));
+                } else {
+                    Distances.Add(new KeyValuePair<string, int>(node.Name, int.MaxValue));
+                    PriorityQueue.Add(new KeyValuePair<string, int>(node.Name, int.MaxValue));
+                }
+                Previous.Add(new KeyValuePair<string, string>(node.Name, null));
+            }
+            PriorityQueue.Sort((x, y) => x.Value.CompareTo(y.Value));
+            //As long as there's smth to visit
+            while (PriorityQueue.Count > 0) {
+                var lowest = PriorityQueue[0].Key;
+                PriorityQueue.RemoveAt(0);
+                if (lowest == end) {
+                    //We're DONE;
+                    Console.WriteLine("Distances");
+                    foreach (var item in Distances) {
+                        Console.WriteLine(item.Key + ":" + item.Value);
+                    }
+                    Console.WriteLine("Previous:");
+                    foreach (var item in Previous) {
+                        Console.WriteLine(item.Key + ":" + item.Value);
+                    }
+                    var path = PrintShortestPath(end, start, "");
+                    path.Reverse();
+                    Console.WriteLine($"Shortest path from {start} to {end} is: {end}-->{path}");
+                    break;
+                };
+                if (lowest != null || Distances
+                    .Where(x => x.Key == lowest)
+                    .Select(x => x.Value)
+                    .FirstOrDefault() != int.MaxValue) {
+                    foreach (var neighbor in Vertexes[lowest].Edges) {
+                        //Find neighboring nodes
+                        var index = Vertexes[lowest].Edges.IndexOf(neighbor);
+                        var nextNode = Vertexes[lowest].Edges[index];
+                        //calculate new distance to neighboring node
+                        var candidate = Distances.Where(x => x.Key == lowest).FirstOrDefault();
+                        var addDistance = candidate.Value + nextNode.Value;
+                        var nextNeighbor = nextNode.Key;
+                        if (addDistance < Distances.Where(x => x.Key == nextNeighbor).Select(x => x.Value).FirstOrDefault()) {
+                            var update = Distances.Where(x => x.Key == nextNeighbor).FirstOrDefault();
+                            //updating new smallest distance to neighbor
+                            var newPair = new KeyValuePair<string, int>(update.Key, addDistance);
+                            Distances.Remove(update);
+                            Distances.Add(newPair);
+                            //update Previous - how we got to neighbor
+                            var previous = Previous.Where(x => x.Key == update.Key).FirstOrDefault();
+                            var newPreviousPair = new KeyValuePair<string, string>(previous.Key, lowest);
+                            Previous.Remove(previous);
+                            Previous.Add(newPreviousPair);
+                            //update the priority queue
+                            var dequeue = PriorityQueue.Where(x => x.Key == nextNeighbor).FirstOrDefault();
+                            var newPriority = new KeyValuePair<string, int>(nextNeighbor, addDistance);
+                            PriorityQueue.Remove(dequeue);
+                            PriorityQueue.Add(newPriority);
+
+                            PriorityQueue.Sort((x, y) => x.Value.CompareTo(y.Value));
+                        }
+                    }
+                }
+            }
+        }
+        public string PrintShortestPath(string end, string start, string path) {
+            if (end == start) {
+                return path.Remove(path.Length - 3);
+               
+            };
+            var endNode = Previous.Where(x => x.Key == end).FirstOrDefault();
+            path += endNode.Value + "-->";
+            return PrintShortestPath(endNode.Value, start, path);
+
+        }
+        public List<KeyValuePair<string, int>> GetOrderedPriority() {
+
+            return PriorityQueue.OrderBy(x => x.Value).ToList();
+        }
+        #endregion
+
+    }
+}
---
+++ Dijkstra/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dijkstra
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var g = new Graph<string>();
+
+            g.AddVertex("A");
+            g.AddVertex("B");
+            g.AddVertex("C");
+            g.AddVertex("D");
+            g.AddVertex("E");
+            g.AddVertex("F");
+
+            g.AddEdge("A", "B", 4);
+            g.AddEdge("A", "C", 2);
+            g.AddEdge("B", "E", 3);
+            g.AddEdge("C", "D", 2);
+            g.AddEdge("C", "F", 4);
+            g.AddEdge("D", "F", 1);
+            g.AddEdge("D", "E", 3);
+
+
+            g.Dijkstra("A","D");
+
+
+            foreach (var item in g.Vertexes.Values) {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine("====");
+            foreach (var item in g.Vertexes.Values) {
+                foreach (var edge in item.Edges) {
+                    Console.WriteLine(item.Name + "=" + edge.Key + "-" + edge.Value);
+                }
+            }
+            Console.WriteLine("===");
+            foreach (var item in g.Distances) {
+                Console.WriteLine(item.Key + ":" + item.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
